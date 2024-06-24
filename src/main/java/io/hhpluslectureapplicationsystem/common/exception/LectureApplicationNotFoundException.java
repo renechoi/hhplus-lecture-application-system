612c7093c1cd458641281f44050aa996a2a64cf6
renechoi <115696395+renechoi@users.noreply.github.com>
2024-06-25 01:25:45 +0900

    feat: LectureApplicationNotFoundException

---
+++ src/main/java/io/hhpluslectureapplicationsystem/common/exception/LectureApplicationNotFoundException.java
@@ -0,0 +1,8 @@
+package io.hhpluslectureapplicationsystem.common.exception;
+
+/**
+ * @author : Rene Choi
+ * @since : 2024/06/23
+ */
+public class LectureApplicationNotFoundException extends RuntimeException{
+}
